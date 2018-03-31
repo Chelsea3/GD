07ada3fc0e65d3460267209ba4ac42268ad45820
Chelsea3 <33610590+Chelsea3@users.noreply.github.com>
2018-03-31 22:30:46 +0800

    MoveEnemy.cs
    这个是在enemy的prefab里加的

--- a.cpp
+++ a.cpp
@@ -1,2 +1,40 @@
-#include <iostream>
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class MoveEnemy : MonoBehaviour {
+
+	//[HideInInspector]
+	public GameObject [] wayPoints;
+	private int currentWayPoint=0;
+	private float lastWayPointSwitchTime;
+	public float speed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		lastWayPointSwitchTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 startPosition = wayPoints [currentWayPoint].transform.position;
+		Vector3 endPosition = wayPoints [currentWayPoint + 1].transform.position;
+
+		float pathLength = Vector3.Distance (startPosition, endPosition);
+		float timeForPath = pathLength / speed;
+		float timeOnPath = Time.time - lastWayPointSwitchTime;
+
+		gameObject.transform.position = Vector3.Lerp (startPosition, endPosition, timeOnPath / timeForPath);
+
+		if (gameObject.transform.position.Equals (endPosition)) {
+			if (currentWayPoint < wayPoints.Length - 2) {
+				currentWayPoint++;
+				lastWayPointSwitchTime = Time.time;
+			}
+			else {
+				Destroy (gameObject);
+			}
+		}
+	}
+}
