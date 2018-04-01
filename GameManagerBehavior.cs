846eb5ccc8bd9d2a148050951bb2af2b122e4788
why1015 <33192439+why1015@users.noreply.github.com>
2018-04-02 00:03:07 +0800

    Create GameManagerBehavior.cs

---
+++ GameManagerBehavior.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerBehavior : MonoBehaviour {
+
+	public Text nextWaveWarning;
+	public Text gameWaves;
+
+	public bool gameOver = false;
+
+	private int wave;
+
+	public IEnumerator returnWarning(){
+		nextWaveWarning.enabled = true;
+		yield return new WaitForSeconds(2);
+		nextWaveWarning.enabled = false;
+	}
+
+	public int Wave {
+		get { 
+			return wave; 
+		}
+		set {
+			wave = value;
+			if (!gameOver) {
+				nextWaveWarning.enabled = true;
+				gameWaves.text = "Wave: " + (wave + 1);
+			}
+			else {
+				gameWaves.text = "Game Over!";
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		Wave = 0;
+		nextWaveWarning.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameObject.Find ("enemy(Clone)") != null) {
+			nextWaveWarning.enabled = false;
+		}
+	}
+}
