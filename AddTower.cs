38d30d1d18b09a99b5a1a434a6db37c9740d7ae9
Chelsea3 <Chelsea3@users.noreply.github.com>
2018-10-17 21:11:04 +0800

    Added some scripts

---
+++ AddTower.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddTower : MonoBehaviour {
+
+	public GameObject towerPrefab;
+	private GameObject tower;
+
+	private bool canPlaceMonster() { 
+		print ("innnnn");
+		return tower == null;
+	}
+
+	// 1
+	void OnMouseUp() 
+	{
+		// 2
+		if (canPlaceMonster()) 
+		{
+			// 3
+			tower = (GameObject)Instantiate(towerPrefab, transform.position, Quaternion.identity);
+			print ("cannnnn");
+			// 4
+			//AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+			//audioSource.PlayOneShot(audioSource.clip); 
+
+			// todo: Deduct gold
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TowerData.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]  
+public class TowerLevel
+{
+	public int cost; 
+	public GameObject visualization;
+	public GameObject bullet;
+	public float fireRate;
+}
+
+public class TowerData : MonoBehaviour {
+
+	public List<TowerLevel> levels;
+	private TowerLevel currentLevel;
+
+	public TowerLevel CurrentLevel 
+	{
+		get { return currentLevel; }
+
+		set 
+		{
+			currentLevel = value;
+			int currentLevelIndex = levels.IndexOf(currentLevel);
+
+			GameObject levelVisualization = levels[currentLevelIndex].visualization;
+			for (int i = 0; i < levels.Count; i++)
+			{
+				if (levelVisualization != null)
+				{
+					if (i == currentLevelIndex)
+					{
+						levels[i].visualization.SetActive(true);
+					}
+					else 
+					{
+						levels[i].visualization.SetActive(false);
+					}
+				}
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
